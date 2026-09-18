b88779a0c7dd28dfbb313d366d186397734c7548
zeldxh <783613+zeldxh@users.noreply.github.com>
2026-09-17 23:07:47 -0600

    test(app): add context-load smoke test for cmed application

---
+++ src/test/java/com/citamed/CmedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.citamed;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CmedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
